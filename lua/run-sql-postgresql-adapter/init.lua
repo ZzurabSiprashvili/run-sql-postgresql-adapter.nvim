dab091a8506fcf7f3a644161d802b611c8331058
ZzurabSiprashvili <zz.siprashvili@gmail.com>
2026-03-12 15:53:00 +0400

    Implement query execution with markdown buffer output

--- lua/run-sql-postgresql-adapter/init.lua
+++ lua/run-sql-postgresql-adapter/init.lua
@@ -19,8 +19,78 @@ function M.connect(config)
 end
 
 function M.run(connection, query)
-  -- For now, just log the query
-  vim.notify("PostgreSQL adapter running query:\n" .. query, vim.log.levels.INFO)
+  local config = connection.config
+  
+  -- Build psql command
+  local cmd = string.format(
+    "PGPASSWORD='%s' psql -h %s -p %s -U %s -d %s -c %s",
+    config.password,
+    config.host,
+    config.port,
+    config.user,
+    config.database,
+    vim.fn.shellescape(query)
+  )
+  
+  -- Execute the query
+  local output = vim.fn.system(cmd)
+  local exit_code = vim.v.shell_error
+  
+  -- Create or reuse buffer for results
+  local bufname = "SQL Results"
+  local bufnr = vim.fn.bufnr(bufname)
+  
+  if bufnr == -1 then
+    -- Create new buffer
+    bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(bufnr, bufname)
+  end
+  
+  -- Set buffer options
+  vim.bo[bufnr].filetype = "markdown"
+  vim.bo[bufnr].buftype = "nofile"
+  vim.bo[bufnr].modifiable = false
+  
+  -- Prepare markdown content
+  local lines = {}
+  table.insert(lines, "# SQL Query Results")
+  table.insert(lines, "")
+  table.insert(lines, "**Connection:** " .. connection.name)
+  table.insert(lines, "**Database:** " .. config.database)
+  table.insert(lines, "")
+  table.insert(lines, "## Query")
+  table.insert(lines, "```sql")
+  for line in query:gmatch("[^\r\n]+") do
+    table.insert(lines, line)
+  end
+  table.insert(lines, "```")
+  table.insert(lines, "")
+  
+  if exit_code == 0 then
+    table.insert(lines, "## Results")
+    table.insert(lines, "```")
+    for line in output:gmatch("[^\r\n]+") do
+      table.insert(lines, line)
+    end
+    table.insert(lines, "```")
+  else
+    table.insert(lines, "## Error")
+    table.insert(lines, "```")
+    table.insert(lines, output)
+    table.insert(lines, "```")
+  end
+  
+  -- Set buffer content
+  vim.bo[bufnr].modifiable = true
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  vim.bo[bufnr].modifiable = false
+  vim.bo[bufnr].modified = false
+  
+  -- Open buffer in a split
+  vim.cmd("vsplit")
+  vim.api.nvim_set_current_buf(bufnr)
+  
+  vim.notify("\nQuery executed")
 end
 
 return M
