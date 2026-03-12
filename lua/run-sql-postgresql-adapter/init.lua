22aaaa126dc526681900cba15277ef70ce96daf4
ZzurabSiprashvili <zz.siprashvili@gmail.com>
2026-03-12 15:23:31 +0400

    Add get_config_schema for adapter-specific parameters

--- lua/run-sql-postgresql-adapter/init.lua
+++ lua/run-sql-postgresql-adapter/init.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+function M.get_config_schema()
+  return {
+    { name = "host", prompt = "Host: ", default = "localhost" },
+    { name = "port", prompt = "Port: ", default = "5432" },
+    { name = "database", prompt = "Database: " },
+    { name = "user", prompt = "User: " },
+    { name = "password", prompt = "Password: " },
+  }
+end
+
 function M.connect(config)
   -- config contains: host, port, database, user, password
   return {
